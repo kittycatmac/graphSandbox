e055cdc8696d6c755c74dfa520a1cfaec067ae4a
Cat <madcatmac16@gmail.com>
2020-01-27 20:23:59 -0700

    svg

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import * as CanvasJS  from 'canvasjs';
+//import * as CanvasJS  from 'canvasjs';
 
 @Component({
   selector: 'app-chart',
@@ -11,55 +11,55 @@ export class ChartComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.showChart();
+    //this.showChart();
   }
   showChart() {
-    var chart = new CanvasJS.Chart("chartContainer", {
+//     var chart = new CanvasJS.Chart("chartContainer", {
+//       animationEnabled: true,
+//       theme: "light2",
+//       exportEnabled: true,      
+//       title:{
+//         text: "Fruits sold in First & Second Quarter"              
+//       },
 
-      theme: "light2",
-            
-      title:{
-        text: "Fruits sold in First & Second Quarter"              
-      },
+//       data: [  //array of dataSeries     
+//       { //dataSeries - first quarter
+//  /*** Change type "column" to "bar", "area", "line" or "pie"***/        
+//        type: "column",
+//        name: "First Quarter",
+//        showInLegend: true,
+//        dataPoints: [
+//        { label: "banana", y: 58 },
+//        { label: "orange", y: 69 },
+//        { label: "apple", y: 80 },                                    
+//        { label: "mango", y: 74 },
+//        { label: "grape", y: 64 }
+//        ]
+//      },
 
-      data: [  //array of dataSeries     
-      { //dataSeries - first quarter
- /*** Change type "column" to "bar", "area", "line" or "pie"***/        
-       type: "column",
-       name: "First Quarter",
-       showInLegend: true,
-       dataPoints: [
-       { label: "banana", y: 58 },
-       { label: "orange", y: 69 },
-       { label: "apple", y: 80 },                                    
-       { label: "mango", y: 74 },
-       { label: "grape", y: 64 }
-       ]
-     },
+//      { //dataSeries - second quarter
 
-     { //dataSeries - second quarter
+//       type: "column",
+//       name: "Second Quarter", 
+//       showInLegend: true,               
+//       dataPoints: [
+//       { label: "banana", y: 63 },
+//       { label: "orange", y: 73 },
+//       { label: "apple", y: 88 },                                    
+//       { label: "mango", y: 77 },
+//       { label: "grape", y: 60 }
+//       ]
+//     }
+//     ],
+//     /** Set axisY properties here*/
+//         axisY:{
+//           prefix: "$",
+//           suffix: "K",
+//           includeZero: false
+//         }    
+//     });
 
-      type: "column",
-      name: "Second Quarter", 
-      showInLegend: true,               
-      dataPoints: [
-      { label: "banana", y: 63 },
-      { label: "orange", y: 73 },
-      { label: "apple", y: 88 },                                    
-      { label: "mango", y: 77 },
-      { label: "grape", y: 60 }
-      ]
-    }
-    ],
-    /** Set axisY properties here*/
-        axisY:{
-          prefix: "$",
-          suffix: "K",
-          includeZero: false
-        }    
-    });
-
-    chart.render();
+//     chart.render();
   }
 }
 
--- src/app/graph/graph.component.ts
+++ src/app/graph/graph.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import * as c3 from 'c3';
+//import * as c3 from 'c3';
 
 @Component({
   selector: 'app-graph',
@@ -11,19 +11,19 @@ export class GraphComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.ngAfterViewInit();
+    //this.ngAfterViewInit();
   }
 
-  ngAfterViewInit() {
-    let chart = c3.generate({
-    bindto: '#chart',
-        data: {
-            columns: [
-                ['data1', 30, 200, 100, 400, 150, 250],
-                ['data2', 50, 20, 10, 40, 15, 25]
-            ]
-        }
-    });
-  }
+  // ngAfterViewInit() {
+  //   let chart = c3.generate({
+  //   bindto: '#chart',
+  //       data: {
+  //           columns: [
+  //               ['data1', 30, 200, 100, 400, 150, 250],
+  //               ['data2', 50, 20, 10, 40, 15, 25]
+  //           ]
+  //       }
+  //   });
+  // }
 
 }
